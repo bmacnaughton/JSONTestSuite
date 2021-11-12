e148c1a064125e6827c9efbe0c1e85755c14fde7
Bruce A. MacNaughton <bruce.macnaughton@contrastsecurity.com>
2021-11-12 06:48:13 -0800

    wip

--- tests/string-parser.rs
+++ tests/string-parser.rs
@@ -13,15 +13,18 @@ type TT = TokenType;
 //    r#"  {"face": "bruce", ["one", "two", "three", "😂", 1.8e23]}"#,
 //];
 
-const TEXT: &str = r#"  {"face": "bruce", "array": ["one", "t\"wo", "three", "😂", 1.8e23, true, null]}"#;
+const TEXT: &str = r#"  {"face": "bruce", "array": {"faked": ["one", "t\"wo", "three", "😂", 1.8e23, true, null]}, "tail": "end"}"#;
+const T2: &str = r#"{"face": "bruce", "one": {"two": {"three": {"last": 5}}}}"#;
+const T3: &str = r#"{"one":{"a":[{"x": 1},{"y": 2}, {"z": 3}]}, "two": 3}"#;
 
 #[test]
 fn visualize() {
-    println!("{}", TEXT.len());
-    for token in Lexer::new(TEXT.bytes(), BufferType::Span) {
+    let input = fs::read_to_string(&"./tests/index-3-issue-38.json").unwrap();
+    println!("{}", input.len());
+    for token in Lexer::new(input.bytes(), BufferType::Span) {
         match token.buf {
             Buffer::Span(span) => {
-                let token_text = &TEXT[span.first as usize..span.end as usize];
+                let token_text = &input[span.first as usize..span.end as usize];
                 println!("{:?} from {} to {}: {}", token.kind, span.first, span.end, token_text);
             },
             _ => println!("{:?}", token.buf)
@@ -31,7 +34,8 @@ fn visualize() {
 
 #[test]
 fn test_parser() -> Result<(), Box<dyn std::error::Error>> {
-    let bytes: String = fs::read_to_string(&"./tests/issue-38.json")?;
+    //let bytes: String = fs::read_to_string(&"./tests/issue-38.json")?;
+    let bytes: String = fs::read_to_string(&"./tests/index-3-issue-38.json")?;
     string_parser(&bytes);
 
     Ok(())
@@ -60,14 +64,14 @@ enum State<'a> {
 enum ObjectState {
     MaybeKey,
     NeedColon,
-    NeedValue,
+    NeedAtom,
     MaybeMore,
     NeedKey,
 }
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 enum ArrayState {
-    MaybeValue,
+    MaybeAtom,
     MaybeMore,
 }
 
@@ -94,30 +98,14 @@ struct Parser {
     //state_stack: &'a Vec<State<'a>>,
 }
 
-fn tok_string(input: &str, token: &Token) -> String {
-    match &token.buf {
-        Buffer::Span(span) => {
-            let token_text = &input[span.first as usize..span.end as usize];
-            String::from(token_text)
-        },
-        _ => format!("{:?}", token.buf)
-    }
-
-    //String::from(&input.[token.buf.first..token.buf.end])
-}
-
 impl Parser {
     fn parse(&mut self, input: &str) {
         let mut path = Vec::<Token>::new();
-        //let state_stack = StateStack {stack: &mut Vec::<State>::new()};
-        //let state_stack = &mut Vec::<State>::new();
-        let mut state_stack: Vec::<State> = vec![State::ExpectNothing];
-
         let mut state: State = State::ExpectAtom;
+        let mut state_stack: Vec::<State> = vec![State::ExpectNothing];
         let mut prev_state: State = state;
 
-
-        let mut pop: bool = false;
+        let mut pop_state: bool = false;
 
         let mut lex_iterator = Lexer::new(input.bytes(), BufferType::Span).peekable();
 
@@ -126,18 +114,26 @@ impl Parser {
             if let Some(token) = lex_iterator.next() {
                 tok = token;
             } else {
+                // can't set state to State::Done because tok is not defined
+                // in this loop. break results in the last state being left on
+                // the stack; it's checked at the end.
                 //state = State::Done;
                 break;
             }
 
+            println!("(token {})", tok_string(input, &tok));
+
+            // continue 'same_token effectively turns the previous lex_iterator.next()
+            // into a peek.
             'same_token: loop {
-                if pop {
-                    pop = false;
+                if pop_state {
+                    pop_state = false;
                     let pstate = state;
                     state = state_stack.pop().unwrap();
-                    println!("  popping {:?} => {:?}", pstate, state);
+                    //println!("  popping {:?} => {:?}", pstate, state);
                 }
-                println!("{:?} => {:?} with {:?}", prev_state, state, tok_string(input, &tok));
+                //print_path("cur", input, &path);
+                //println!("{:?} => {:?} with {:?}", prev_state, state, tok_string(input, &tok));
                 prev_state = state;
 
                 match state {
@@ -145,12 +141,12 @@ impl Parser {
                         match tok.kind {
                             TT::BooleanTrue | TT::BooleanFalse | TT::Null | TT::Number => {
                                 // have standalone value that we don't care about
-                                pop = true;
+                                pop_state = true;
                             },
                             TT::String => {
                                 // TODO scan string for badness and then nothing more should be
                                 // present.
-                                pop = true;
+                                pop_state = true;
                             },
                             TT::CurlyOpen => {
                                 // now parsing an object, possibly empty
@@ -158,7 +154,7 @@ impl Parser {
                             },
                             TT::BracketOpen => {
                                 // parse an array, possibly empty
-                                state = State::Array(ArrayState::MaybeValue);
+                                state = State::Array(ArrayState::MaybeAtom);
                             },
                             _ => {
                                 state = State::BadJson("unexpected @ ExpectAtom");
@@ -172,13 +168,19 @@ impl Parser {
                                     TT::String => {
                                         // TODO scan string key for badness
                                         path.push(tok.clone());
+                                        println!("pushed path on String in ObjectState::MaybeKey {}", path.len());
                                         state = State::Object(ObjectState::NeedColon);
                                     },
                                     TT::CurlyClose => {
                                         // pop state. i don't think it's possible for
                                         // the stack to be empty because we're in a state
                                         // that implies something is on the stack.
-                                        pop = true;
+                                        pop_state = true;
+                                        path.pop().unwrap();
+                                        if path.is_empty() {
+                                            println!("path was emptied on {:?} {}", tok, tok_string(input, &tok));
+                                        }
+                                        println!("popped path on CurlyClose in ObjectState::MaybeKey {}", path.len());
                                     },
                                     _ => {
                                         state = State::BadJson("key or close brace required");
@@ -187,47 +189,34 @@ impl Parser {
                             },
                             ObjectState::NeedColon => {
                                 if tok.kind == TT::Colon {
-                                    state = State::Object(ObjectState::NeedValue);
+                                    state = State::Object(ObjectState::NeedAtom);
                                 } else {
                                     state = State::BadJson("missing colon");
                                 }
                             },
-                            ObjectState::NeedValue => {
+                            ObjectState::NeedAtom => {
                                 state_stack.push(State::Object(ObjectState::MaybeMore));
                                 state = State::ExpectAtom;
+                                println!("  re-using {:?}", tok.kind);
                                 continue 'same_token;
-                                /*
-                                match tok.kind {
-                                    TT::String => {
-                                        // TODO scan string
-                                        state = State::Object(ObjectState::MaybeMore);
-                                    },
-                                    TT::BooleanTrue | TT::BooleanFalse | TT::Number => {
-                                        state = State::Object(ObjectState::MaybeMore);
-                                    },
-                                    TT::CurlyOpen => {
-                                        state_stack.push(state);
-                                        state = State::Object(ObjectState::MaybeKey);
-
-                                    },
-                                    TT::BracketOpen => {
-                                        state_stack.push(state);
-                                        state = State::Array(ArrayState::MaybeValue);
-                                    },
-                                    _ => {
-                                        state = State::BadJson("key requires value");
-                                    }
-                                }
-                                // */
                             },
                             ObjectState::MaybeMore => {
                                 match tok.kind {
                                     TT::Comma => {
                                         path.pop().unwrap();
+                                        if path.is_empty() {
+                                            println!("path was emptied on {:?} {}", tok, tok_string(input, &tok));
+                                        }
+                                        println!("popped path on Comma in ObjectState::MaybeMore {}", path.len());
                                         state = State::Object(ObjectState::NeedKey);
                                     },
                                     TT::CurlyClose => {
-                                        pop = true;
+                                        pop_state = true;
+                                        path.pop().unwrap();
+                                        if path.is_empty() {
+                                            println!("path was emptied on {:?} {}", tok, tok_string(input, &tok));
+                                        }
+                                        println!("popped state and path on CurlyClose in ObjectState::MaybeMore {}", path.len());
                                     },
                                     _ => {
                                         state = State::BadJson("expected comma or right brace");
@@ -239,6 +228,7 @@ impl Parser {
                                     TT::String => {
                                         // TODO scan string for badness
                                         path.push(tok.clone());
+                                        println!("pushed path on String in ObjectState::NeedKey {}", path.len());
                                         state = State::Object(ObjectState::NeedColon);
                                     },
                                     _ => {
@@ -250,14 +240,15 @@ impl Parser {
                     },
                     State::Array(array_state) => {
                         match array_state {
-                            ArrayState::MaybeValue => {
+                            ArrayState::MaybeAtom => {
                                 match tok.kind {
                                     TT::BracketClose => {
-                                        pop = true;
+                                        pop_state = true;
                                     },
                                     _ => {
                                         state_stack.push(State::Array(ArrayState::MaybeMore));
                                         state = State::ExpectAtom;
+                                        println!("  re-using {:?}", tok.kind);
                                         continue 'same_token;
                                     },
                                 }
@@ -269,7 +260,7 @@ impl Parser {
                                         state = State::ExpectAtom;
                                     },
                                     TT::BracketClose => {
-                                        pop = true;
+                                        pop_state = true;
                                     },
                                     _ => {
                                         state = State::BadJson("expected comma or right bracket");
@@ -304,5 +295,37 @@ impl Parser {
         }
 
         println!("path len {:?}", path.len());
+        if path.len() < 10 {
+            print_path("final", input, &path);
+        }
     }
+}
+
+
+fn tok_string(input: &str, token: &Token) -> String {
+    match &token.buf {
+        Buffer::Span(span) => {
+            let token_text = &input[span.first as usize..span.end as usize];
+            let s = String::from(token_text);
+            if s.chars().count() > 60 {
+                s.chars().into_iter().take(60).collect()
+            } else {
+                s
+            }
+        },
+        _ => format!("{:?}", token.buf)
+    }
+}
+
+fn print_path(text: &str, input: &str, path: &[Token]) {
+    if path.len() > 10 {
+        println!("  path len {}", path.len());
+        return;
+    }
+    print!("  {} path: ", text);
+    for token in path.iter() {
+        let s = tok_string(input, token);
+        print!("{}.", s);
+    }
+    println!();
 }
